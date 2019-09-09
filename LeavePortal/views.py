82d12815d906e5b910bd1b725521fcdc630d5f4e
Chaitanya Suvarna <chaitanyasuvarna@Chaitanyas-MacBook-Air.local>
2019-09-09 21:03:36 +0530

    Made changes in template, used bootstrap to display days/months

--- LeavePortal/views.py
+++ LeavePortal/views.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
-
 from django.template import RequestContext
+import calendar
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html',{})
+    month_names = ['Jan','Feb','Mar','Apr','May','June','Jul','Aug','Sep','Oct','Nov','Dec']
+    months=[]
+    for i in range(1,13):
+        months.append(Month(month_names[i-1],range(1,calendar.monthrange(2019,i)[1]+1)))
+    return render(request,'index.html',{'months':months})
+
+class Month:
+    def __init__(self, name, daycount):
+        self.name = name
+        self.daycount = daycount
