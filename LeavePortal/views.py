5ff3869124a38e915fda0921f99b44132a8f56ca
Chaitanya Suvarna <chaitanyasuvarna@Chaitanyas-MacBook-Air.local>
2019-12-31 20:40:46 +0530

    If Resource/Leave_date combinayion already exists, Leave will be updated else Leave will be created. Added success and error messages in Leave form.

--- LeavePortal/views.py
+++ LeavePortal/views.py
@@ -25,15 +25,25 @@ def index(request):
     return render(request,'index.html',{'months':months, 'names':names, 'leaves':leaves, 'resource_leaves':resource_leaves, 'year':datetime.datetime.now().year})
 
 def leaves(request):
+    success_message=''
     if request.method == 'POST':
         form = LeaveForm(request.POST)
         if form.is_valid():
-            print('VALID INPUT')
-            form.save()
+            obj_count = Leave.objects.filter(leave_date=form.cleaned_data['leave_date'], resource=form.cleaned_data['resource']).count()
+            if obj_count == 0:
+                # A new leave object created
+                form.save()
+                success_message = 'Leave added successfully!'
+            else:
+                # leave object already exists
+                leave_obj = Leave.objects.get(leave_date=form.cleaned_data['leave_date'], resource=form.cleaned_data['resource'])
+                leave_obj.leave_type = form.cleaned_data['leave_type']
+                leave_obj.save()
+                success_message = 'Leave updated successfully!'
         else:
             return render(request,'leaves.html',{'form':form})
-    form=LeaveForm()
-    return render(request,'leaves.html',{'form':form})
+    form=LeaveForm(initial={'leave_date': 'DD/MM/YYYY'})
+    return render(request,'leaves.html',{'form':form,'success_message':success_message})
 
 class Month:
     def __init__(self, name, daycount, number):
